3e485a5eb52350ce2f6422d528ba3514eed74557
barneyman <barnaby.james.flint@gmail.com>
2023-04-14 23:18:38 +0000

    handle device removal

--- ha_test.py
+++ ha_test.py
@@ -2,6 +2,7 @@ from pycec.cec import CecAdapter
 from pycec.network import HDMINetwork, PhysicalAddress
 from pycec.tcp import TcpAdapter
 import time
+import datetime
 
 initialised=False
 devices=[]
@@ -15,9 +16,17 @@ def test_ha(host,display_name):
     def _new_device(device):
         global devices
         devices.append(device)
-        print("New device {} {}".format(device.name, device.type_name))
+        # name is the only thing valid at this point
+        print("{} New device {}".format(datetime.datetime.now(), device.name))
+
+    def _device_removed(exit_device):
+        global devices
+        devices=[device for device in devices if device.name != exit_device.name]
+        print("{} remove device {} {}".format(datetime.datetime.now(), exit_device.name, exit_device.type_name))
+
 
     hdmi_network.set_new_device_callback(_new_device)
+    hdmi_network.set_device_removed_callback(_device_removed)
     
     def _initialised():
         global initialised
@@ -32,9 +41,8 @@ def test_ha(host,display_name):
 
     while True:
         for device in devices:
-            print("device {} type {} state {}".format(device.name,device.type_name,"ON" if device.is_on else "OFF"))
-        pass
-        time.sleep(30)
+            print("{} device {} type {} state {}".format(datetime.datetime.now(), device.name,device.type_name,"ON" if device.is_on else "OFF"))
+        time.sleep(120)
 
 
 test_ha("hassio","rubble")
